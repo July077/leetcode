d2a9324550da5411aa5ddf33d56c0ef377ff9102
sunjianrong <sunjianrong0707@foxmail.com>
2021-06-06 23:08:38 +0800

    44. 通配符匹配

---
+++ java/com.suny.leetcode/Q44_Wildcard_Matching.java
@@ -0,0 +1,45 @@
+/**
+ * 给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+ * <p>
+ * '?' 可以匹配任何单个字符。
+ * '*' 可以匹配任意字符串（包括空字符串）。
+ * 两个字符串完全匹配才算匹配成功。
+ * <p>
+ * 说明:
+ * <p>
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021/6/6 下午10:47
+ */
+public class Q44_Wildcard_Matching {
+
+    public boolean isMatch(String s, String p) {
+        int len1 = p.length();
+        int len2 = s.length();
+
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= len1; i++) {
+            if (p.charAt(i - 1) != '*') {
+                break;
+            }
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(i - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+                }
+            }
+
+        }
+
+        return dp[len1][len2];
+    }
+}
