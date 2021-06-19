30176159542fada237a9e278a08adcd8de61e6ef
sunjianrong <sunjianrong0707@foxmail.com>
2021-06-19 23:15:01 +0800

    63. 不同路径 II

---
+++ java/com.suny.leetcode/Q63不同路径II.java
@@ -0,0 +1,45 @@
+/**
+ * 63. 不同路径 II
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ *
+ * @author sunjianrong
+ * @date 2021/6/19 下午11:03
+ */
+public class Q63不同路径II {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+
+        // 定义 dp 数组并初始化第一行第一列
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n && obstacleGrid[0][i] == 0; i++) {
+            dp[0][i] = 1;
+        }
+
+        // 根据 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] 进行递推。
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+
+        }
+
+        return dp[m - 1][n - 1];
+
+    }
+
+}
