005213999b0abf918cc910e07eae869d42d936ab
sunjianrong <sunjianrong0707@foxmail.com>
2021-07-09 23:09:15 +0800

    121. 买卖股票的最佳时机

---
+++ java/com.suny.leetcode/Q121买卖股票的最佳时机.java
@@ -0,0 +1,49 @@
+/**
+ * 121. 买卖股票的最佳时机
+ * <p>
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * <p>
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * @author sunjianrong
+ * @date 2021/7/9 下午10:57
+ */
+public class Q121买卖股票的最佳时机 {
+
+
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+
+    // 暴力解法  n2
+    public int maxProfit0(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+
+    }
+
+}
