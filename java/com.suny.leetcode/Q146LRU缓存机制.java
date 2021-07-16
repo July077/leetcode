16d86b06359c20cddab34bd8327e46fb002c1eb1
sunjianrong <sunjianrong0707@foxmail.com>
2021-07-16 12:15:53 +0800

    146. LRU 缓存机制

---
+++ java/com.suny.leetcode/Q146LRU缓存机制.java
@@ -0,0 +1,41 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 146. LRU 缓存机制
+ * <p>
+ * 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
+ * 实现 LRUCache 类：
+ * <p>
+ * LRUCache(int capacity) 以正整数作为容量capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value)如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ *
+ * <p>
+ * 进阶：你是否可以在O(1) 时间复杂度内完成这两种操作？
+ *
+ * @author sunjianrong
+ * @date 2021-07-16 12:07
+ */
+public class Q146LRU缓存机制 extends LinkedHashMap<Integer, Integer> {
+
+    private int capacity;
+
+    public Q146LRU缓存机制(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
