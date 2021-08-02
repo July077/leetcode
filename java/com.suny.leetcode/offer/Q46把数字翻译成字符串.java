f0328c1479b2356095b56baf1856dde55a735f24
sunjianrong <sunjianrong0707@foxmail.com>
2021-08-02 22:22:38 +0800

    剑指 Offer 46. 把数字翻译成字符串

---
+++ java/com.suny.leetcode/offer/Q46把数字翻译成字符串.java
@@ -0,0 +1,55 @@
+package offer;
+
+/**
+ * 剑指 Offer 46. 把数字翻译成字符串
+ * <p>
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 12258
+ * 输出: 5
+ * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021/8/2 下午9:48
+ */
+public class Q46把数字翻译成字符串 {
+
+    public int translateNum0(int num) {
+        String s = String.valueOf(num);
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            String substring = s.substring(i - 2, i);
+            if (substring.compareTo("10") >= 0 && substring.compareTo("25") <= 0) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+
+        return dp[s.length()];
+
+    }
+
+    public int translateNum(int num) {
+        String s = String.valueOf(num);
+        int a = 1;
+        int b = 1;
+        for (int i = 2; i <= s.length(); i++) {
+            String tmp = s.substring(i - 2, i);
+            int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+            b = a;
+            a = c;
+        }
+
+        return a;
+
+    }
+}
