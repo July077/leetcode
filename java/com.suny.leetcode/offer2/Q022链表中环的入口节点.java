32ccf562c401bf1f8e6344d11958445c3a4902a5
sunjianrong <sunjianrong0707@foxmail.com>
2021-08-14 22:56:07 +0800

    剑指 Offer II 022. 链表中环的入口节点

---
+++ java/com.suny.leetcode/offer2/Q022链表中环的入口节点.java
@@ -0,0 +1,38 @@
+package offer2;
+
+import common.ListNode;
+
+/**
+ * 剑指 Offer II 022. 链表中环的入口节点
+ * 给定一个链表，返回链表开始入环的第一个节点。 从链表的头节点开始沿着 next 指针进入环的第一个节点为环的入口节点。如果链表无环，则返回null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ * <p>
+ * 说明：不允许修改给定的链表。
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021/8/14 下午10:30
+ */
+public class Q022链表中环的入口节点 {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            while (fast == slow) {
+                ListNode index1 = head;
+                ListNode index2 = fast;
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+
+        return null;
+    }
+}
