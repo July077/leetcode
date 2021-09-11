d1b6f049270cdc8b1c79c446215de314654e2d67
sunjianrong <sunjianrong0707@foxmail.com>
2021-09-11 16:10:58 +0800

    剑指 Offer II 103. 最少的硬币数目

---
+++ java/com.suny.leetcode/offer2/Q103最少的硬币数目.java
@@ -0,0 +1,67 @@
+package offer2;
+
+/**
+ * 剑指 Offer II 103. 最少的硬币数目
+ * <p>
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回-1。
+ * <p>
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ *
+ * <p>
+ * 示例1：
+ * <p>
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
+ * <p>
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021/9/11 下午2:59
+ */
+public class Q103最少的硬币数目 {
+
+    int res = Integer.MAX_VALUE;
+    int[] memo;
+
+    public int coinChange(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+        memo = new int[amount];
+
+        return dfs(coins, amount);
+    }
+
+    private int dfs(int[] coins, int amount) {
+        if (amount < 0) {
+            return -1;
+        }
+
+        if (amount == 0) {
+            return 0;
+        }
+        if (memo[amount - 1] != 0) {
+            return memo[amount - 1];
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+            int res = dfs(coins, amount - coins[i]);
+            if (res >= 0 && res < min) {
+                min = res + 1;
+            }
+        }
+
+        memo[amount - 1] = (min == Integer.MAX_VALUE ? -1 : min);
+        return memo[amount - 1];
+    }
+}
