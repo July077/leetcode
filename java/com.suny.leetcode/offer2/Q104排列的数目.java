f1097680e9d96c4b02486d7b680a24b362135617
sunjianrong <sunjianrong0707@foxmail.com>
2021-09-11 16:35:30 +0800

    剑指 Offer II 104. 排列的数目

---
+++ java/com.suny.leetcode/offer2/Q104排列的数目.java
@@ -0,0 +1,48 @@
+package offer2;
+
+/**
+ * 剑指 Offer II 104. 排列的数目
+ * <p>
+ * 给定一个由 不同正整数组成的数组 nums ，和一个目标整数 target 。请从 nums 中找出并返回总和为 target 的元素组合的个数。数组中的数字可以在一次排列中出现任意次，但是顺序不同的序列被视作不同的组合。
+ * <p>
+ * 题目数据保证答案符合 32 位整数范围。
+ * <p>
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3], target = 4
+ * 输出：7
+ * 解释：
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * 请注意，顺序不同的序列被视作不同的组合。
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021/9/11 下午4:30
+ */
+public class Q104排列的数目 {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+
+        return dp[target];
+
+    }
+
+}
