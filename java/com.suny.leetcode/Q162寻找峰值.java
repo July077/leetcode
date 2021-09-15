5a433eed211dbd3b40fa8339115ee715f9c11072
sunjianrong <sunjianrong0707@foxmail.com>
2021-09-15 21:43:51 +0800

    162. 寻找峰值

---
+++ java/com.suny.leetcode/Q162寻找峰值.java
@@ -0,0 +1,40 @@
+/**
+ * 162. 寻找峰值
+ * <p>
+ * 峰值元素是指其值严格大于左右相邻值的元素。
+ * <p>
+ * 给你一个整数数组nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+ * <p>
+ * 你可以假设nums[-1] = nums[n] = -∞ 。
+ * <p>
+ * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+ * <p>
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3,1]
+ * 输出：2
+ * 解释：3 是峰值元素，你的函数应该返回其索引 2。
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021-09-15 21:24
+ */
+public class Q162寻找峰值 {
+
+
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        for (; left < right; ) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
