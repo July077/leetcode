ad4b27dd9a7f08f4913ff377e2f274a3d6e163e1
sunjianrong <sunjianrong0707@foxmail.com>
2021-09-25 23:10:32 +0800

    583. 两个字符串的删除操作

---
+++ java/com.suny.leetcode/Q583两个字符串的删除操作.java
@@ -0,0 +1,48 @@
+/**
+ * 583. 两个字符串的删除操作
+ * 给定两个单词word1和word2，找到使得word1和word2相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ * <p>
+ *
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ *
+ * <p>
+ * 提示：
+ * <p>
+ * 给定单词的长度不超过500。
+ * 给定单词中的字符只含有小写字母。
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021-09-25 23:03
+ */
+public class Q583两个字符串的删除操作 {
+
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < word1.length() + 1; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i < word2.length() + 1; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 2, Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                }
+            }
+
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+}
