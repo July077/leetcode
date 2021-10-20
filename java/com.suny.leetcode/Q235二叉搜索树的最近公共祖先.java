9dc332f2b5937b2d59bf1c6ab92869fb5a0540a0
sunjianrong <sunjianrong0707@foxmail.com>
2021-10-20 12:10:55 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ java/com.suny.leetcode/Q235二叉搜索树的最近公共祖先.java
@@ -0,0 +1,34 @@
+import common.TreeNode;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉搜索树: root =[6,2,8,0,4,7,9,null,null,3,5]
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ *
+ * @author sunjianrong
+ * @date 2021-10-20 12:02
+ */
+public class Q235二叉搜索树的最近公共祖先 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        return root;
+    }
+
+}
