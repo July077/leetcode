a56464674b0d89294e62f8877f32a4c29af43382
sunjianrong <sunjianrong0707@foxmail.com>
2021-11-18 09:33:11 +0800

    563. 二叉树的坡度

---
+++ java/com.suny.leetcode/Q563二叉树的坡度.java
@@ -0,0 +1,48 @@
+import common.TreeNode;
+
+/**
+ * 563. 二叉树的坡度
+ * 给定一个二叉树，计算 整个树 的坡度 。
+ * <p>
+ * 一个树的 节点的坡度 定义即为，该节点左子树的节点之和和右子树节点之和的 差的绝对值 。如果没有左子树的话，左子树的节点之和为 0 ；没有右子树的话也是一样。空结点的坡度是 0 。
+ * <p>
+ * 整个树 的坡度就是其所有节点的坡度之和。
+ * <p>
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3]
+ * 输出：1
+ * 解释：
+ * 节点 2 的坡度：|0-0| = 0（没有子节点）
+ * 节点 3 的坡度：|0-0| = 0（没有子节点）
+ * 节点 1 的坡度：|2-3| = 1（左子树就是左子节点，所以和是 2 ；右子树就是右子节点，所以和是 3 ）
+ * 坡度总和：0 + 0 + 1 = 1
+ * <p>
+ *
+ * @author sunjianrong
+ * @date 2021-11-18 9:20
+ */
+public class Q563二叉树的坡度 {
+
+    private int abs = 0;
+
+    public int findTilt(TreeNode root) {
+        dfs(root);
+        return abs;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        abs += Math.abs(left - right);
+        return left + right + root.val;
+    }
+}
