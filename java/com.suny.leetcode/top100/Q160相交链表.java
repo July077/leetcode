285c58d2948f5572f5e7e0e2b0d9f1ae55d518aa
sunjianrong <sunjianrong0707@foxmail.com>
2022-02-17 13:08:09 +0800

    Q160相交链表

---
+++ java/com.suny.leetcode/top100/Q160相交链表.java
@@ -0,0 +1,18 @@
+package top100;
+
+import common.ListNode;
+
+/**
+ * 
+ */
+public class Q160相交链表 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA;
+        ListNode B = headB;
+        while (A != B) {
+            A = A != null ? A.next : headB;
+            B = B != null ? B.next : headA;
+        }
+        return A;
+    }
+}
