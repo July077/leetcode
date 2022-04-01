7fac791b34fc00246d19f10128e89856b9e70745
sunjianrong <sunjianrong0707@foxmail.com>
2022-04-01 23:07:01 +0800

    Q206反转链表

---
+++ java/com.suny.leetcode/top100/Q206反转链表.java
@@ -0,0 +1,31 @@
+
+/**
+ * 206. 反转链表
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 示例 2：
+ */
+public class Q206反转链表 {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+
+}
