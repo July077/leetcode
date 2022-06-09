48b82ee113574b01b4a1409c3399991e65c573d7
sunjianrong <sunjianrong0707@foxmail.com>
2022-06-09 09:31:48 +0800

    岛屿数量

---
+++ java/com.suny.leetcode/top100/Q200岛屿数量.java
@@ -0,0 +1,79 @@
+package top100;
+
+/**
+ * 200. 岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：grid = [
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 示例 2：
+ * 
+ * 输入：grid = [
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ * 
+ * 
+ * 提示：
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ */
+public class Q200岛屿数量 {
+
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        // 判断是否在格子里面
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int x, int y) {
+        if (!inArea(grid, x, y)) {
+            return;
+        }
+
+        if (grid[x][y] != '1') {
+            return;
+        }
+
+        // 标识已经访问过
+        grid[x][y] = '2';
+
+        // 访问上下左右
+        dfs(grid, x - 1, y);
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y - 1);
+        dfs(grid, x, y + 1);
+    }
+
+    private boolean inArea(char[][] grid, int x, int y) {
+        return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length;
+    }
+
+}
